6681e98c2db81840ad352e8a8c6272282ad46864
nexoscp <nexoscp@users.noreply.github.com>
2019-09-12 19:39:20 +0200

    add source roots from libdeps-dir

--- src/main/kotlin/platformio/Startup.kt
+++ src/main/kotlin/platformio/Startup.kt
@@ -13,38 +13,29 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.util.PsiTreeUtil.findChildrenOfType
 import ini4idea.lang.psi.IniSection
 
-fun  <T1, T2, T3, T4> tuple(t1: T1, t2: T2, t3: T3, t4: T4):NTuple4<T1, T2, T3, T4> {
-    return NTuple4(t1, t2, t3, t4)
-}
-data class NTuple4<T1, T2, T3, T4>(val t1: T1, val t2: T2, val t3: T3, val t4: T4)
-
-infix fun <T1, T2, T3, T4> Triple<T1, T2, T3>.then(t4: T4): NTuple4<T1, T2, T3, T4> {
-    return NTuple4(this.first, this.second, this.third, t4)
-}
-
 class Startup : StartupActivity {
     override fun runActivity(project: Project) {
-        VirtualFileManager.getInstance().addVirtualFileListener(object: VirtualFileListener {
+        VirtualFileManager.getInstance().addVirtualFileListener(object : VirtualFileListener {
             override fun propertyChanged(event: VirtualFilePropertyEvent) {
-                if(event.fileName == PlatformIO.INI) {
+                if (event.fileName == PlatformIO.INI) {
                     updateOnINI(project, event.file)
                 }
             }
 
             override fun fileCreated(event: VirtualFileEvent) {
-                if(event.fileName == PlatformIO.INI) {
+                if (event.fileName == PlatformIO.INI) {
                     updateOnINI(project, event.file)
                 }
             }
 
             override fun fileCopied(event: VirtualFileCopyEvent) {
-                if(event.fileName == PlatformIO.INI) {
+                if (event.fileName == PlatformIO.INI) {
                     updateOnINI(project, event.file)
                 }
             }
 
             override fun contentsChanged(event: VirtualFileEvent) {
-                if(event.fileName == PlatformIO.INI) {
+                if (event.fileName == PlatformIO.INI) {
                     updateOnINI(project, event.file)
                 }
             }
@@ -52,21 +43,21 @@ class Startup : StartupActivity {
 
         val app = ApplicationManager.getApplication()
         app.invokeLater {
-        app.runWriteAction {
-            ModuleManager.getInstance(project).modules
-                    .map { Pair(it, ModuleRootManager.getInstance(it)) }
-                    .flatMap { (module, rootManager) -> module.rootManager.contentRoots.asIterable().map { Triple(it, module, rootManager) } }
-                    .map { (root, module, rootManager) -> tuple(root, root.findChild(PlatformIO.INI), module, rootManager) }
-                    .filter { (_, ini, _, _) -> ini != null && ini.exists() }
-                    .map { (root, ini, module, rootManager) -> tuple(root, PsiManager.getInstance(project).findFile(ini!!), module, rootManager)}
-                    .filter { (_, psi, _, _) -> psi != null}
-                    .forEach { (root, psi, module, rootManager) -> markDirs(root, psi!!, module, rootManager) }
-        }
+            app.runWriteAction {
+                ModuleManager.getInstance(project).modules
+                        .map { Pair(it, ModuleRootManager.getInstance(it)) }
+                        .flatMap { (module, rootManager) -> module.rootManager.contentRoots.asIterable().map { Triple(it, module, rootManager) } }
+                        .map { (root, module, rootManager) -> tuple(root, root.findChild(PlatformIO.INI), module, rootManager) }
+                        .filter { (_, ini, _, _) -> ini != null && ini.exists() }
+                        .map { (root, ini, module, rootManager) -> tuple(root, PsiManager.getInstance(project).findFile(ini!!), module, rootManager) }
+                        .filter { (_, psi, _, _) -> psi != null }
+                        .forEach { (root, psi, module, rootManager) -> markDirs(root, psi!!, module, rootManager) }
+            }
         }
     }
 }
 
-fun updateOnINI(project:Project, file:VirtualFile) {
+fun updateOnINI(project: Project, file: VirtualFile) {
     val app = ApplicationManager.getApplication()
     app.invokeLater {
         app.runWriteAction {
@@ -83,7 +74,7 @@ fun updateOnINI(project:Project, file:VirtualFile) {
     }
 }
 
-fun markDirs(root:VirtualFile, psi: PsiFile, module: Module, rootManager: ModuleRootManager) {
+fun markDirs(root: VirtualFile, psi: PsiFile, module: Module, rootManager: ModuleRootManager) {
     var srcDirName: String = "src"
     var testSrcDirName: String = "test"
     findChildrenOfType(psi, IniSection::class.java).forEach { iniSection ->
@@ -102,7 +93,7 @@ fun markDirs(root:VirtualFile, psi: PsiFile, module: Module, rootManager: Module
     val model = rootManager.modifiableModel
     val content = model.addContentEntry(root)
     val srcDir = root.findChild(srcDirName)
-    if(srcDir != null
+    if (srcDir != null
             && srcDir.exists()
             && srcDir.isDirectory
             && !rootManager.sourceRoots.contains(srcDir)
@@ -111,13 +102,27 @@ fun markDirs(root:VirtualFile, psi: PsiFile, module: Module, rootManager: Module
         content.addSourceFolder(srcDir, false)
     }
     val testSrcDir = root.findChild(testSrcDirName)
-    if(testSrcDir != null
+    if (testSrcDir != null
             && testSrcDir.exists()
             && testSrcDir.isDirectory
             && !rootManager.sourceRoots.contains(testSrcDir)
             && !rootManager.excludeRoots.contains(testSrcDir)) {
 
         content.addSourceFolder(testSrcDir, true)
+    }
+    //walk thrue .pio/libdeps/*/*/ and add them as sourceroots
+    val libdeps = root.findChild(".pio")?.findChild("libdeps")
+    libdeps?.children?.forEach { libs -> run {
+            libs?.children?.forEach { l -> run {
+                    val src = l.findChild("src")
+                    if (src == null) {
+                        content.addSourceFolder(l, false)
+                    } else {
+                        content.addSourceFolder(src, false)
+                    }
+                }
+            }
+        }
     }
     model.commit()
 }

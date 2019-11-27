94aa34b3a30ae5f00e6f996a5b2acb0b7f5410d6
Alex Fedorov <afedorov@amazon.com>
2019-11-26 20:56:20 -0800

    Show proper error message if PlatformIO CLI is not installed

--- src/test/kotlin/platformio/services/impl/CommandLinePlatformIOServiceTest.kt
+++ src/test/kotlin/platformio/services/impl/CommandLinePlatformIOServiceTest.kt
@@ -4,6 +4,7 @@ import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.greaterThan
 import org.junit.Assert.assertThat
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class CommandLinePlatformIOServiceTest {
     private val commandLinePlatformIOService = CommandLinePlatformIOService()
@@ -12,5 +13,10 @@ class CommandLinePlatformIOServiceTest {
     fun loadsBoards() {
         val loadAllBoards = commandLinePlatformIOService.loadAllBoards()
         assertThat(loadAllBoards.size, `is`(greaterThan(500)))
+    }
+
+    @Test
+    fun `Platform CLI is available`() {
+        assertTrue(commandLinePlatformIOService.isAvailable())
     }
 }
